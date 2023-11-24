676a1bd69669fffb7daef759387b5fd79532718f
Adham <adham00109@gmail.com>
2023-11-24 15:31:01 +0800

    update client

--- server/connectDB.js
+++ server/connectDB.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection(process.env.MYSQL_CONNECTION_URI);
-// export const db = mysql.createConnection({
-//     host:process.env.MYSQL_DB_HOST,
-//     user: process.env.MYSQL_DB_USER,
-//     password: process.env.MYSQL_DB_PASSWORD,
-//     database: process.env.MYSQL_DB_NAME,
-// });
+// export const db = mysql.createConnection(process.env.MYSQL_CONNECTION_URI);
+export const db = mysql.createConnection({
+    host:process.env.MYSQL_DB_HOST,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+});
