9157f70e965cad05534df805ca57acdf310884ec
Adham <adham00109@gmail.com>
2023-11-24 16:32:25 +0800

    update server

--- server/index.js
+++ server/index.js
@@ -26,6 +26,8 @@ const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.INTERNAL_CLIENT_URL);
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(express.urlencoded({ extended: false }));
